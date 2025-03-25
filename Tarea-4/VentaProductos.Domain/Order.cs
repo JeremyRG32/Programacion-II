91a75edae2b2b7ddae5ab6bd7302d07419b8b8fa
JeremyRG32 <Jeremyrg619@gmail.com>
2025-03-24 22:15:50 -0400

    Agregue Tarea 4

---
+++ Tarea-4/VentaProductos.Domain/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace VentaProductos.Domain
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer? Customer { get; set; }
+
+        public int ProductId { get; set; }
+        public Product? Product { get; set; }
+    }
+}
---
+++ Tarea-4/VentaProductos.Persistence/Data/VentaProductosDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VentaProductos.Domain;
+
+namespace VentaProductos.Persistence.Data
+{
+    public class VentaProductosDbContext : DbContext
+    {
+        public VentaProductosDbContext(DbContextOptions<VentaProductosDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
